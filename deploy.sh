e246a5225d425cf6ffad0b24302167c645d21c32
Frank Taylor <7483580+shagamemnon@users.noreply.github.com>
2019-03-06 20:41:20 -0500

    added function name variable

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,14 @@
 BUCKET_NAME="my_cloudflarelogs_gcp_storage_bucket"
 DATASET="cloudflare_logs"
 TABLE="cloudflare_logs"
+FUNCTIONS=("gcsbq")
 
-gcloud functions deploy gcsbq \
-    --runtime nodejs8 \
-    --trigger-resource $BUCKET_NAME \
-    --trigger-event google.storage.object.finalize \
-    --memory=1024MB \
-    --set-env-vars DATASET=$DATASET,TABLE=$TABLE,SCHEMA="./schema.json"
+for FUNC in $FUNCTIONS
+do
+    gcloud functions deploy $FUNC \
+      --runtime nodejs8 \
+      --trigger-resource $BUCKET_NAME \
+      --trigger-event google.storage.object.finalize \
+      --memory=1024MB \
+      --set-env-vars DATASET=$DATASET,TABLE=$TABLE,SCHEMA="./schema.json"
+done
