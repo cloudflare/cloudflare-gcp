f7eaeee035e9886b583e0ef64ff19e9d4ba9abf0
Frank Taylor <franklin@cloudflare.com>
2020-01-15 04:56:41 -0500

    updated readme and removed unused files

--- cli/utils/paths.js
+++ cli/utils/paths.js
@@ -1,29 +1,25 @@
 const path = require('path')
 const findUp = require('find-up')
-const globalDirs = require('global-dirs')
 
-console.log(globalDirs.npm.prefix)
 // globalDirs.npm.binaries
 // findUp.sync(filename, [options])
 
 const links = {
   get baseDir () {
     delete this.baseDir
-    this.baseDir = path.join(findUp.sync('utils'), '..')
+    this.baseDir = path.join(findUp.sync('cloudflare-gcp'), 'cli')
     return this.baseDir
   },
   get deploymentDir () {
     delete this.deploymentDir
-    this.deploymentDir = path.join(findUp.sync('utils'), '..', '..', 'security-events')
+    this.deploymentDir = path.join(findUp.sync('cloudflare-gcp'), 'security-events')
     return this.deploymentDir
   },
   get envDir () {
     delete this.envDir
-    this.envDir = path.join(findUp.sync('utils'), '..', '..', 'security-events', '.env.yml')
+    this.envDir = path.join(findUp.sync('cloudflare-gcp'), 'security-events', '.env.yml')
     return this.envDir
   }
 }
-
-console.log(links.deploymentDir, links.baseDir, links.envDir)
 
 module.exports = links
