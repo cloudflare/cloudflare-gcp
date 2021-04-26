9b0e07fe14aa7ca6edfb3322b2b0922aadf43fc0
Frank Taylor <franklin@cloudflare.com>
2021-04-26 15:25:54 -0400

    modified function to use pubsub and cron trigger

--- logpush-to-bigquery/cloudshell.md
+++ logpush-to-bigquery/cloudshell.md
@@ -1,21 +1,30 @@
 # Cloudflare Log Push
 
 ## Select project
+
 <walkthrough-project-setup></walkthrough-project-setup>
 
 ## Set project ID
-```
+
+```sh
 gcloud config set project {{project-id}}
 ```
 
 ## Update the environment variables in deploy.sh
+
 ```sh
 sudo nano deploy.sh
 ```
+
 #### Variable reference
+
 ```
 # required – The name of Google Cloud Storage bucket used for Cloudflare Logpush logs.
 BUCKET_NAME=""
+
+# required – The name of the subdirectory in your bucket used for Cloudflare Logpush logs,
+# for example, "logs/". If there is no subdirectory, use "/"
+DIRECTORY="/"
 
 # optional - specify a different schema. Spectrum users will want to change this to
 # schema-spectrum.json.
@@ -29,9 +38,14 @@ TABLE=""
 
 # optional - the name of your Cloud Function. default: gcsbq
 FN_NAME=""
+
+# optional - the name of the pubsub topic that will be published every minute
+TOPIC_NAME="every_minute"
+
 ```
 
 ## Deploy to GCP
+
 ```sh
 sh ./deploy.sh
 ```
--- logpush-to-bigquery/index.js
+++ logpush-to-bigquery/index.js
@@ -1,22 +1,18 @@
-'use strict'
-
-const { BigQuery } = require('@google-cloud/bigquery')
 const { Storage } = require('@google-cloud/storage')
-const bigquery = new BigQuery()
+const { BigQuery } = require('@google-cloud/bigquery')
+const { Logging } = require('@google-cloud/logging')
+const { DateTime } = require('luxon')
+
 const storage = new Storage()
+const bigquery = new BigQuery()
+const logging = new Logging()
+const log = logging.log('logpush-to-bigquery-sink')
+const bucket = storage.bucket(process.env.BUCKET_NAME)
 
-async function gcsbq (file, context) {
+async function gcsbq (files) {
   const schema = require(`./${process.env.SCHEMA}`)
-
   const datasetId = process.env.DATASET
   const tableId = process.env.TABLE
-
-  console.log(`Starting job for ${file.name}`)
-
-  if (file.name.includes('tableMeta')) return
-
-  const filename = storage.bucket(file.bucket).file(file.name)
-
   /* Configure the load job and ignore values undefined in schema */
   const metadata = {
     sourceFormat: 'NEWLINE_DELIMITED_JSON',
@@ -26,17 +22,75 @@ async function gcsbq (file, context) {
     ignoreUnknownValues: true
   }
 
-  const addToTable = async (tableId) => {
+  const addToTable = async tableId => {
     const dataset = await bigquery.dataset(datasetId).get({ autoCreate: true })
     const table = await dataset[0].table(tableId).get({ autoCreate: true })
-    return table[0].load(filename, metadata)
+    return table[0].createLoadJob(files, metadata)
   }
 
   try {
-    await addToTable(tableId)
+    return addToTable(tableId)
   } catch (e) {
     console.log(e)
   }
 }
 
-module.exports.gcsbq = gcsbq
+async function writeLog (logData) {
+  logData = logData.join(',\n')
+
+  const metadata = {
+    resource: { type: 'global' },
+    severity: 'INFO'
+  }
+
+  const entry = log.entry(metadata, logData)
+  await log.write(entry)
+  console.log(`Logged: ${logData}`)
+}
+
+module.exports.runLoadJob = async function (message, context) {
+  if (!context) {
+    context = {}
+    context.timestamp = new Date().toISOString()
+  }
+  context.timestamp = DateTime.fromISO(context.timestamp)
+
+  const loadJobDeadline = context.timestamp
+    .setZone('GMT')
+    .minus({ minutes: 3 })
+    .startOf('minute')
+
+  const [deadlineDate, deadlineTime] = [
+    loadJobDeadline.toFormat('yyyyMMdd'),
+    loadJobDeadline.toFormat(`yyyyMMdd'T'hhmm`)
+  ]
+
+  let arrayOfLogpushLogs = []
+  let stackdriverEntry = []
+
+  try {
+    bucket
+      .getFilesStream({
+        autoPaginate: false,
+        maxResults: 5000,
+        prefix: `${process.env.DIRECTORY}${deadlineDate}/${deadlineTime}`
+      })
+      .on('error', console.error)
+      .on('data', function (file) {
+        let logEndTime = file.name.split('_').slice(1, -1)
+        logEndTime = DateTime.fromISO(logEndTime, { setZone: true })
+        arrayOfLogpushLogs.push(bucket.file(file.name))
+        stackdriverEntry.push(file.name)
+      })
+      .on('end', async function () {
+        if (arrayOfLogpushLogs.length > 0) {
+          await gcsbq(arrayOfLogpushLogs)
+          await writeLog(stackdriverEntry)
+        } else {
+          console.log(`No new logs at ${deadlineTime}`)
+        }
+      })
+  } catch (e) {
+    console.log(e)
+  }
+}
